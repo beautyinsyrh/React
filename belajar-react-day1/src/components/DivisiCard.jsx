dc97656dce0213d737bd4d00e72ff24dd50cc0fd
Beauty Insyiroh Nusuvul Layli <beautyinsyrhhhh@gmail.com>
2025-07-01 14:31:43 +0700

    tugas 1

--- belajar-react-day1/src/components/DivisiCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function DivisiCard({ nama }) {
-  return (
-    <div className="bg-white border rounded p-4 shadow hover:shadow-md cursor-pointer">
-      <p className="font-medium text-gray-800">{nama}</p>
-    </div>
-  );
-}
